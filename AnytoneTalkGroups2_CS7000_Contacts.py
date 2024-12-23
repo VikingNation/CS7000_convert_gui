761193f95bc39cc0931fa07a00bf78459aab7803
Jason Johnson <k3jsj@arrl.net>
2024-12-23 17:50:32 +0000

    Created class for Digital Contacts and migrated code into the class

--- AnytoneTalkGroups2_CS7000_Contacts.py
+++ AnytoneTalkGroups2_CS7000_Contacts.py
@@ -1,6 +1,6 @@
 #!/bin/env python
-import csv
 import sys
+from DigitalContacts import DigitalContacts
 
 # Output file columns
 # No, Call Alias, Call Type, Call ID, Receive Tone
@@ -14,24 +14,12 @@ if len(sys.argv) != 3:
     print("")
     sys.exit(1)
 
+
 # Get input and output file names from arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# Open the input file for reading
-with open(input_file, mode='r', newline='') as infile:
-    reader = csv.reader(infile)
-    
-    # Open the output file for writing
-    with open(output_file, mode='w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        
-        # Process each row in the input file
-        for row in reader:
-            number = row[0]
-            radio_id = row[1]
-            call_alias = row[2]
-            call_type = row[3]
-            receive_tone = row[4]
-            writer.writerow([number, call_alias, call_type, radio_id, receive_tone])
 
+# 
+contacts = DigitalContacts(input_file, output_file)
+contacts.Convert()
---
+++ DigitalContacts.py
@@ -0,0 +1,66 @@
+import csv
+from hashlib import sha256
+
+class DigitalContacts:
+
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+
+        self.__fileType = ''
+        self.__DetermineFileType()
+
+    def Convert(self):
+        if self.__fileType == "Anytone":
+            self.ConvertAnytoneTalkGroups()
+
+    def ConvertAnytoneTalkGroups(self):
+        input_file = self.input_file
+        output_file = self.output_file
+
+        print("Input file type is Anytone\nNow converting ", self.__rowsInFile, " talk groups")
+        # Open the input file for reading
+        with open(input_file, mode='r', newline='') as infile:
+            reader = csv.reader(infile)
+            
+            # Open the output file for writing
+            with open(output_file, mode='w', newline='') as outfile:
+                writer = csv.writer(outfile)
+                
+                # Process each row in the input file
+                for row in reader:
+                    number = row[0]
+                    radio_id = row[1]
+                    call_alias = row[2]
+                    call_type = row[3]
+                    receive_tone = row[4]
+                    writer.writerow([number, call_alias, call_type, radio_id, receive_tone])
+
+        outfile.close()
+        infile.close()
+
+        print("Complete!")
+
+    def __DetermineFileType(self):
+
+        with open(self.input_file, mode='r', newline='') as infile:
+         
+            reader = csv.reader(infile)
+            numRows = 0 
+            for row in reader:
+                if numRows == 0:
+                    s = ''
+
+                    for e in enumerate(row):
+                        s = s + e[1]
+
+                    headerHash = sha256(s.encode('utf-8')).hexdigest()
+                   
+                    if (headerHash == "3fcf4f8abf1017013669181c3b25ac2662c989e07fd05330250a6348b55baef2"):
+                        self.__fileType = 'Anytone'
+
+                numRows = numRows + 1
+
+            self.__rowsInFile = numRows
+
+        infile.close()
