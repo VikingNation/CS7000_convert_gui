bbc56ea61ca7a9ec1814c26d9fed7e7bad46dc4f
Jason Johnson <k3jsj@arrl.net>
2024-12-31 21:52:03 -0500

    Update CS7000_convert_gui:  Removed need to specify individual location of Talkgroup, Channel, and Zone file. User now just needs to select input folder with Anytone files and output folder to output spreadsheets for CS7000 application.  Updated the configuration reader/writer to save the user's select of importing digital + analog or just digital channels.

--- CS7000_convert_gui.py
+++ CS7000_convert_gui.py
@@ -12,16 +12,23 @@ from connectSystems.CS7000.Zones import Zones
 #
 # Author: Jason Johnson <k3jsj@arrl.net>
 # Purpose:  GUI to convert Anytone Talkgroups, Channels, and Zones into format for CS7000 CPS
-#           Program prompts for directory and assumes files are called talkgroups.csv, channels.csv, and zones.csv
-#           Program outputs contactsc.csv, channels.csv, and zones.csv into directory selected
+#           Program prompts for directory of Anytone Talkgroups, Channel, and Zone files
+#           Program outputs spreadsheets in format for CS7000
 #
 #           Portions of this source code were generated using Microsoft Co-Pilot
 
+def select_input_directory():
+    directory = filedialog.askdirectory(title="Chose directory for Anytone input files")
+    if directory:
+        input_directory_var.set(f"Selected directory: {directory}")
+        debug_output('User selected input directory')
+
+
 def select_output_directory():
-    directory = filedialog.askdirectory(title="Chose directorty to output files")
+    directory = filedialog.askdirectory(title="Chose directory to output CS7000 spreadsheet files")
     if directory:
         output_directory_var.set(f"Selected directory: {directory}")
-        debug_output('User selected contacts file')
+        debug_output('User selected output directory')
 
 
 def open_file_explorer(path):
@@ -33,24 +40,6 @@ def open_file_explorer(path):
         os.system(f"xdg-open {path}")
     else:
         print("Unsupported operating system")
-
-def select_contacts_file():
-    file = filedialog.askopenfilename(title="Select contacts file to import")
-    if file:
-        contacts_file_var.set(f"Selected file: {file}")
-        debug_output('User selected contacts file')
-
-def select_channels_file():
-    file = filedialog.askopenfilename(title="Select channels file to import")
-    if file:
-        channels_file_var.set(f"Selected file: {file}")
-        debug_output('User selected channels file')
-
-def select_zones_file():
-    file = filedialog.askopenfilename(title = "Select zones file to import")
-    if file:
-        zones_file_var.set(f"Selected file: {file}")
-        debug_output('User selected zones file')
 
 def getFilename(s):
     return s.replace("Selected file: ", "")
@@ -64,34 +53,35 @@ def debug_output(text):
 def convert_codeplug():
     debug_output('Now converting files into format for CS7000')
 
+    input_directory = getDirectoryname(input_directory_var.get())
     output_directory = getDirectoryname(output_directory_var.get())
 
+    contacts_file = input_directory + "/Talkgroups.CSV"
+    channels_file = input_directory + "/Channel.CSV"
+    zones_file = input_directory + "/Zone.CSV"
 
     # Convert contact file
-    converted_contacts_file = output_directory + "/CS7000_contacts.csv"
-    contacts = DigitalContacts(getFilename(contacts_file_var.get()), converted_contacts_file)
+    converted_contacts_file = output_directory + "/CS7000_contacts.xlsx"
+    contacts = DigitalContacts(contacts_file, converted_contacts_file)
     contacts.Convert()
     debug_output("Converted talkgroup file in " + converted_contacts_file)
 
     # Convert channels file
-    if ( channel_type.get() == "digital_analog"):
+    if ( channel_type_var.get() == "digital_analog"):
         includeAnalogChannels = True
     else:
         includeAnalogChannels = False
 
     debug_output("Including analog channels in conversion is " + str(includeAnalogChannels))
 
-    converted_ch_dmr_file = output_directory + "/CS7000_channels_dmr.csv"
-    converted_ch_analog_file = output_directory + "/CS7000_channels_analog.csv"
-    channels = Channels(getFilename(channels_file_var.get()), converted_ch_dmr_file, converted_ch_analog_file, includeAnalogChannels)
+    converted_channels_file = output_directory + "/CS7000_channels.xlsx"
+    channels = Channels(channels_file, converted_channels_file, includeAnalogChannels)
     uhfChannels = channels.Convert()
-    debug_output("Converted dmr channels to " +  converted_ch_dmr_file)
-    debug_output("Converted analog channels to " +  converted_ch_analog_file)
+    debug_output("Converted channels to " +  converted_channels_file)
 
     # Convert zones file
-    zone_file = getFilename(zones_file_var.get())
-    converted_zones_file = output_directory + "/CS7000_zones.csv"
-    zones = Zones(getFilename(zones_file_var.get()), converted_zones_file, uhfChannels)
+    converted_zones_file = output_directory + "/CS7000_zones.xlsx"
+    zones = Zones(zones_file, converted_zones_file, uhfChannels)
     zones.Convert()
     debug_output("Converted zones file in " + converted_zones_file)
     
@@ -99,15 +89,13 @@ def convert_codeplug():
     debug_output("Now opening folder containing converted output files")
 
 def exit_application():
-    contacts_file = contacts_file_var.get().replace("Selected file: ", "")
-    channels_file = channels_file_var.get().replace("Selected file: ", "")
-    zones_file = zones_file_var.get().replace("Selected file: ", "")
+    input_directory = getDirectoryname(input_directory_var.get())
     output_directory = getDirectoryname(output_directory_var.get())
-
+    channel_type = channel_type_var.get()
     with open('CS7000_convert_settings.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Contacts File', 'Channels File', 'Zone File', 'Output Directory'])
-        writer.writerow([contacts_file, channels_file, zones_file, output_directory])
+        writer.writerow(['Input Directory', 'Output Directory', 'Channel Type'])
+        writer.writerow([input_directory, output_directory, channel_type])
    
     file.close()
     root.destroy()
@@ -122,20 +110,17 @@ def read_csv_and_set_variables(file_path):
         variables = {header: value for header, value in zip(headers, data)}
 
         # Set variables
-        contacts_file = variables.get('Contacts File', '')
-        channels_file = variables.get('Channels File', '')
-        zones_file = variables.get('Zone File', '')
+        input_directory = variables.get('Input Directory', '')
         output_directory = variables.get('Output Directory', '')
-
-        contacts_file_var.set(f"Selected file: {contacts_file}")
-        channels_file_var.set(f"Selected file: {channels_file}")
-        zones_file_var.set(f"Selected file: {zones_file}")
+        channel_type = variables.get('Channel Type', '')
+        input_directory_var.set(f"Selected directory: {input_directory}")
         output_directory_var.set(f"Selected directory: {output_directory}")
+        channel_type_var.set(f"{channel_type}")
         debug_output('Read settings from CS7000_convert_setting.csv')
 
 # Create the main application window
 root = tk.Tk()
-root.title("CS7000 Code Plug Utility - By Jason Johnson <k3jsj@arrl.net>  Version 1.1")
+root.title("CS7000 Code Plug Utility - By Jason Johnson (K3JSJ) <k3jsj@arrl.net>  Version 1.2")
 
 
 
@@ -152,43 +137,35 @@ frame = ttk.LabelFrame(left_frame, text="Import channels")
 frame.pack(padx=10, pady=10, fill="x")
 
 # Create radio button variables
-channel_type = tk.StringVar(value="digital")
+channel_type_var = tk.StringVar(value="digital")
 
 # Create radio buttons
-radio_digital = ttk.Radiobutton(frame, text="Digital", variable=channel_type, value="digital")
-radio_digital_analog = ttk.Radiobutton(frame, text="Digital & Analog", variable=channel_type, value="digital_analog")
+radio_digital = ttk.Radiobutton(frame, text="Digital", variable=channel_type_var, value="digital")
+radio_digital_analog = ttk.Radiobutton(frame, text="Digital & Analog", variable=channel_type_var, value="digital_analog")
 
 # Pack radio buttons
 radio_digital.pack(anchor="w", padx=10, pady=5)
 radio_digital_analog.pack(anchor="w", padx=10, pady=5)
 
 # Create StringVar variables to hold files paths
-contacts_file_var = tk.StringVar()
-channels_file_var = tk.StringVar()
-zones_file_var = tk.StringVar()
+input_directory_var = tk.StringVar()
 output_directory_var = tk.StringVar()
 
 
 # Create buttons
-btn_select_contacts = ttk.Button(left_frame, text="Select Contacts File", command=select_contacts_file)
-btn_select_channels = ttk.Button(left_frame, text="Select Channels File", command=select_channels_file)
-btn_select_zone = ttk.Button(left_frame, text="Select Zone File", command=select_zones_file)
-
-btn_select_output_dir = ttk.Button(left_frame, text="Select Output directory", command=select_output_directory)
-
+btn_select_input_dir = ttk.Button(left_frame, text="Select input directory", command=select_input_directory)
+btn_select_output_dir = ttk.Button(left_frame, text="Select output directory", command=select_output_directory)
 
 btn_convert_codeplug = ttk.Button(left_frame, text="Convert Codeplug", command=convert_codeplug)
 btn_exit = ttk.Button(left_frame, text="Exit", command=exit_application)
 
 # Pack buttons and labels
-btn_select_contacts.pack(padx=10, pady=5, fill="x")
-ttk.Label(left_frame, textvariable=contacts_file_var).pack(padx=10, pady=5, fill="x")
-btn_select_channels.pack(padx=10, pady=5, fill="x")
-ttk.Label(left_frame, textvariable=channels_file_var).pack(padx=10, pady=5, fill="x")
-btn_select_zone.pack(padx=10, pady=5, fill="x")
-ttk.Label(left_frame, textvariable=zones_file_var).pack(padx=10, pady=5, fill="x")
+btn_select_input_dir.pack(padx=10, pady=5, fill="x")
+ttk.Label(left_frame, textvariable=input_directory_var).pack(padx=10, pady=5, fill="x")
+
 btn_select_output_dir.pack(padx=10, pady=5, fill="x")
 ttk.Label(left_frame, textvariable=output_directory_var).pack(padx=10, pady=5, fill="x")
+
 btn_convert_codeplug.pack(padx=10, pady=5, fill="x")
 btn_exit.pack(padx=10, pady=5, fill="x")
 
