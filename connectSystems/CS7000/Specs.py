5261514edb960a7c337eaa998f0eb524cf397d93
Jason Johnson <k3jsj@arrl.net>
2025-02-08 23:33:44 -0500

    Updated specs to read values from a json file.  Added utility class to verify signature of files.

--- connectSystems/CS7000/Specs.py
+++ connectSystems/CS7000/Specs.py
@@ -1,14 +1,27 @@
+import json
+from util.verifySignature import verifySignature
 class Specs:
 
     def __init__(self):
-        self.specs = {
-            "9.00.05": {
-                "maxContacts": 2000,
-                "maxChannels": 2000,
-                "maxChannelsPerZone": 160,
-                "maxZones" : 250
-            }
-        }
+
+        # Specify the path to your JSON file
+        json_file_path = 'cs7000.specs.json'
+        json_file_sig = 'cs7000.specs.json.sig'
+
+        v = verifySignature()
+        if v.verifyFile (json_file_path, json_file_sig):
+            print ("Signature of " + json_file_path + " is valid.")
+        else:
+            print ("Signature of " + json_file_path + " is invalid.")
+
+
+        # Open the JSON file and load its contents into a dictionary
+        with open(json_file_path, 'r') as json_file:
+            self.specs = json.load(json_file)
+
+        # Print the dictionary to verify the contents
+        print(self.specs)
+        json_file.close()
 
     def printSpecs(self, firmwareVersion):
         # Accessing the device_info for firmware 3.0.1
